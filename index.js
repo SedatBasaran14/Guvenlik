39685d3a93d495fb693da3ea9ae7c6f3fb3b4a2e
unknown <>
2021-02-28 21:58:07 +0300

    v 0.2

--- index.js
+++ index.js
@@ -1,69 +1,146 @@
-require('dotenv').config()
+require('dotenv').config();
 const express    = require("express");
 const bodyParser = require("body-parser");
 const app        = express();
 const mongoose   = require("mongoose");
-const encrypt    = require("mongoose-encryption")
-
+const session    = require("express-session");
+const passport   = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require("mongoose-findorcreate");
+//const bcrypt     = require('bcrypt');
+//const saltRounds = 10;
+//const md5        = require("md5");
+//const encrypt    = require("mongoose-encryption");
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded( {extended: true} ));
-app.use(express.static(__dirname + "/dosyalar"));
-
-
+app.use(express.static(__dirname + "/public"));
+app.use(session({
+  secret: 'Techproeducation - Web Developerlar',
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 mongoose.connect(process.env.BAGLANTI, {useNewUrlParser: true , useUnifiedTopology : true});
-
+mongoose.set("useCreateIndex", true);
 const Schema = mongoose.Schema;
 const uyeSemasi = new mongoose.Schema ({
-  email : String,
-  sifre : String
+  username : String,
+  sifre : String,
+  googleId : String
 });
-
-
-
-uyeSemasi.plugin(encrypt, {secret : process.env.ANAHTAR , encryptedFields  : ['sifre'] });
-
-
+uyeSemasi.plugin(passportLocalMongoose);
+uyeSemasi.plugin(findOrCreate);
+//uyeSemasi.plugin(encrypt, {secret : process.env.ANAHTAR , encryptedFields  : ['sifre'] });
 const Kullanici = new mongoose.model("Kullanici", uyeSemasi);
-app.get("/", function(req, res){
-  res.render("anasayfa");
+passport.use(Kullanici.createStrategy());
+//*  TARAYICIDA COOKIE OLUŞTURACAĞIZ.         *//
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
 });
+//*  TARAYICIDAN ALDIĞIMIZ COOKİ'İ ÇÖZECEĞİZ' *//
+passport.deserializeUser(function(id, done) {
+  Kullanici.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:5000/auth/google/hosgeldin",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    Kullanici.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+app.get("/", function(req, res){
+  if(req.isAuthenticated()){
+    res.send("sen daha önceden giriş yapmıştın. o yüzden sana farklı bir sayfa göstereceğim");
+  }else{
+    res.render("anasayfa");
+  }
+});
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+app.get('/auth/google/hosgeldin',
+  passport.authenticate('google', { failureRedirect: '/girisyap' }),
+  function(req, res) {
+    res.render("gizlisayfa");
+  });
 app.get("/kayitol", function(req, res){
   res.render("kayitol");
 });
 app.post("/kayitol", function(req, res){
-  const uye = new Kullanici({
-    email : req.body.username,
+  Kullanici.register({username: req.body.username}, req.body.password , function(err, gelenVeri){
+    if(err){
+      console.log(err);
+      res.redirect("/kayitol")
+    }else{
+      passport.authenticate("local")(req,res, function(){
+        res.render("gizlisayfa");
+      });
+    }
+  });
+  /*bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const uye = new Kullanici({
+      email : req.body.username,
+      sifre : hash
+    });
+    uye.save(function(err){
+      if(err)
+        console.log(err);
+      else
+        res.render("gizlisayfa");
+    });
+  });*/
+})
+app.get("/giris", function(req, res){
+  if(req.isAuthenticated()){  // eğer giriş yapılmışsa
+    res.render("gizlisayfa");
+  }else{
+    res.render("giris");
+  }
+});
+app.post("/girisyap", function(req, res){
+  const kullanici = new Kullanici({
+    username : req.body.username,
     sifre : req.body.password
   });
-  uye.save(function(err){
+  req.login(kullanici, function(err){
     if(err)
       console.log(err);
     else
-      res.render("gizlisayfa");
-  });
-})
-app.get("/giris", function(req, res){
-  res.render("giris");
-});
-app.post("/girisyap", function(req, res){
-  const emailGelen = req.body.username;
+      passport.authenticate("local")(req,res, function(){
+        res.render("gizlisayfa");
+      })
+  })
+  /*const emailGelen = req.body.username;
   const sifreGelen = req.body.password;
   Kullanici.findOne({email : emailGelen}, function(err, gelenVeri){
     if(err){
       console.log(err);
     }else{
       if(gelenVeri){ // gelen veri var mı
-        if(gelenVeri.sifre == sifreGelen){
-          res.render("gizlisayfa");
-        }else{
-          res.send("Şifre hatalı");
-        }
+        bcrypt.compare(sifreGelen, gelenVeri.sifre, function(err, result) {
+            if(result) // şifreler aynı
+              res.render("gizlisayfa");
+            else
+              res.send("Şifre hatalı");
+        });
       }else{
         res.send("Böyle bir kullanıcı yok.");
       }
     }
-  });
+  });*/
 })
+app.get("/cikisyap", function(req, res){
+  req.logout();
+  res.redirect("/");
+});
 app.listen(5000, function(){
   console.log("5000 port'a bağlandık.");
 })
